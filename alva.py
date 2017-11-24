f1464e3719eb542bda3c613b6d0a9b467fd64e84
Lauri <lauri.vuorela@gmail.com>
2017-11-24 11:12:39 +0100

    cleaned up code a bit

--- alva.py
+++ alva.py
@@ -1,8 +1,9 @@
-import sys
 import json
-import requests
-import random
+import logging
 import os
+import random
+import requests
+import sys
 
 argument = sys.argv[1] if len(sys.argv) > 1 else None
 
@@ -12,14 +13,12 @@ def post_slack(payload):
     print(response.text)
 
 
-with open(os.path.join(os.path.dirname(__file__), 'webhook.json'),
-          'r') as json_file:
+with open(os.path.join(os.path.dirname(__file__), 'webhook.json'), 'r') as json_file:
     json_data = json.loads(json_file.read())
     url = json_data.get('slack_webhook')
     channel = json_data.get('channel')
 
-with open(os.path.join(os.path.dirname(__file__), 'alva-settings.json'),
-          'r') as messages_file:
+with open(os.path.join(os.path.dirname(__file__), 'alva-settings.json'), 'r') as messages_file:
     json_data = json.loads(messages_file.read())
     msg30 = []
     msg15 = []
@@ -44,7 +43,7 @@ elif argument == 'func2':
 elif argument == 'func3':
     payload_text['text'] = random.choice(msg)
 else:
+    logging.error('faulty argument')
     sys.exit()
 
-payload = json.dumps(payload_text)
-post_slack(payload)
+post_slack(json.dumps(payload_text))
