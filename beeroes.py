e43828f4c27aa80356e3bc35ffe2dc2a91dc9e66
Lauri <lauri.vuorela@gmail.com>
2018-09-21 16:37:51 +0200

    forgot to update beeroes

--- beeroes.py
+++ beeroes.py
@@ -1,15 +1,15 @@
-# -*- coding: utf-8 -*-
-from alva import load_slack_settings, post_slack
+# coding: utf-8
+from alva import FriendlyNeighborhoodBeerBot
 import json
 import logging
 import os
 import pygsheets
 
 
-def create_string(name_list):
-    name_string = None
+def create_string(name_list: list) -> str:
+    name_string = ''
     for i, name in enumerate(name_list, start=1):
-        if name is None or name == '':
+        if not name or name == '':
             continue
         if name_string is None:
             name_string = '@{}'.format(name)
@@ -20,7 +20,7 @@ def create_string(name_list):
     return name_string
 
 
-def set_or_add(d, k, v):
+def set_or_add(d: dict, k: str, v: int) -> None:
     if k not in d:
         d[k] = v
     else:
@@ -28,22 +28,32 @@ def set_or_add(d, k, v):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    url, channel, payload_text = load_slack_settings()
+    logging.basicConfig(
+        level=logging.WARNING,
+        format='%(asctime)s %(levelname)s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
     # google drive/sheets login-credentials-stuff
-    client = pygsheets.authorize(service_file=os.path.join(os.path.dirname(__file__), 'client_secret.json'))
+    client = pygsheets.authorize(
+        service_file=os.path.join(
+            os.path.dirname(__file__),
+            'client_secret.json'
+        )
+    )
 
     # get all beeroes that haven't been thanked before
-    sheet = client.open('alva-beeroes').sheet1  # spreadsheet called alva-beeroes must be created and the service account invited by email to edit
+    # spreadsheet called alva-beeroes must be created
+    # and the service account invited by email to edit
+    sheet = client.open('alva-beeroes').sheet1
     all_records = sheet.get_all_records()
 
     # now that we have ALL THE DATA, do some calculations
-    old_beeroes = {}
-    week_beeroes = []
-    duplicate_check = {}
-    virgins = []
-    veterans = []
+    old_beeroes: dict = {}
+    week_beeroes: list = []
+    duplicate_check: dict = {}
+    virgins: list = []
+    veterans: list = []
     for i, beero in enumerate(all_records, start=2):
         try:
             bname = beero.get('Who')
@@ -61,14 +71,18 @@ if __name__ == '__main__':
                     duplicate_check[bdate].append(bname)
                 else:
                     if bname in duplicate_check.get(bdate):
-                        logging.warning('it seems that {} is trying to cheat!'.format(bname))
-                        sheet.update_cell('C{}'.format(i), '-')  # update column C of the current row
+                        logging.warning(
+                            'it seems that {} is trying to cheat!'.format(bname)
+                        )
+                        # update column C of the current row
+                        sheet.update_cell('C{}'.format(i), '-')
                         continue
                     else:
                         if bdate not in duplicate_check:
                             duplicate_check[bdate] = []
                         duplicate_check[bdate].append(bname)
-                sheet.update_cell('C{}'.format(i), 'X')  # update column C of the current row
+                # update column C of the current row
+                sheet.update_cell('C{}'.format(i), 'X')
                 if bname not in old_beeroes and bname not in virgins:
                     virgins.append(bname)
                 elif old_beeroes.get(bname) == 19:
@@ -82,16 +96,25 @@ if __name__ == '__main__':
 
     # as long as there was someone doing a run this week, thank those brave souls
     if len(week_beeroes) > 0:
-        string_thanks = 'A big thank you to {} for the much appriciated beer run! :clap:'.format(create_string(week_beeroes))
+        string_thanks = 'A big thank you to {} for the much appriciated beer run! :clap:'.format(
+            create_string(week_beeroes)
+        )
 
         # if there was someone who was on their first run, that certainly requires an extra shout out!
         if len(virgins) > 0:
-            string_thanks = '{} And hey! It was also {}\'s first beer run this year! Yeeeeeey!'.format(string_thanks, create_string(virgins))
+            string_thanks += ' And hey! It was also {}\'s first beer run this year! Yeeeeeey!'.format(
+                create_string(virgins)
+            )
 
         # if someone has been on 20 runs total, that is both praise- and badge worthy
         if len(veterans) > 0:
-            string_thanks = '{} ALSO WOW! {} {} been on 20 runs so far! This is the stuff legends are made of!'.format(string_thanks, create_string(veterans), 'has' if len(veterans) == 1 else 'have')
-        payload_text['text'] = string_thanks
+            string_thanks += ' ALSO WOW! {} {} been on 20 runs so far! This is the stuff legends are made of!'.format(
+                create_string(veterans),
+                'has' if len(veterans) == 1 else 'have'
+            )
+        Alva = FriendlyNeighborhoodBeerBot()
+        Alva.load_slack_settings()
+        Alva.payload_text['text'] = string_thanks
 
         # once all is well, post to slack
-        post_slack(url, json.dumps(payload_text))
+        Alva.post_slack(json.dumps(Alva.payload_text))
