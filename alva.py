348f1f5e7c54254ad9dc8883e69ba528313feba9
Lauri <lauri.vuorela@gmail.com>
2018-09-21 14:20:55 +0200

    alva now knows what day it is

--- alva.py
+++ alva.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from typing import Any
 import logging
 import os
 import random
@@ -6,13 +8,13 @@ import sys
 import ujson
 
 
-def post_slack(url, payload):
+def post_slack(url: str, payload: Any) -> str:
     response = requests.post(url, data=payload)
     logging.info('slack response: {}'.format(response.text))
     return response.text
 
 
-def load_slack_settings():
+def load_slack_settings() -> tuple:
     with open(os.path.join(os.path.dirname(__file__), 'webhook.json'), 'r') as json_file:
         json_data = ujson.loads(json_file.read())
         url = json_data.get('slack_webhook')
@@ -26,24 +28,36 @@ def load_slack_settings():
         return url, channel, payload_text
 
 
-def load_messages():
+def replace_variables(message: str) -> str:
+    reps = [
+        ('$weekday', datetime.now().strftime('%A'))
+    ]
+    for rep in reps:
+        message = message.replace(rep[0], rep[1])
+    return message
+
+
+def load_messages() -> tuple:
     with open(os.path.join(os.path.dirname(__file__), 'alva-settings.json'), 'r', encoding='utf-8') as messages_file:
         json_data = ujson.loads(messages_file.read())
         msg30 = []
         msg15 = []
         msg = []
         for message in json_data.get('msg30'):
-            msg30.append(message)
+            msg30.append(replace_variables(message))
         for message in json_data.get('msg15'):
-            msg15.append(message)
+            msg15.append(replace_variables(message))
         for message in json_data.get('msg'):
-            msg.append(message)
+            msg.append(replace_variables(message))
         return msg30, msg15, msg
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    argument = sys.argv[1] if len(sys.argv) > 1 else None
+    try:
+        argument = sys.argv[1]
+    except Exception:
+        argument = 'func3'
     url, channel, payload_text = load_slack_settings()
     msg30, msg15, msg = load_messages()
 
